311ad87a780930aae2300fb18b8ec0dda6bd26d6
atloZ <rekenei.zolta@gmail.com>
2021-09-27 16:48:57 +0200

    db terv

---
+++ Vizsgaremek/Tervek/db.sql
@@ -0,0 +1,23 @@
+felhasználok
+- id
+- user
+- pass
+- aktiv
+- tipus
+- profilkép
+- bio
+
+
+poszt
+- id
+- kat id
+- cim
+- leirás
+- tulajdonos id
+- aktiv
+- kiadásdátum
+
+
+kategoria
+- id
+- nev
